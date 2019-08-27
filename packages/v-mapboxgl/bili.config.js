d02cb55e6bca74c34a321d04b1c0ce0f9083fa92
reno-xjb <23520255+reno-xjb@users.noreply.github.com>
2019-08-27 15:46:26 +0200

    build: drop the rollup-plugin-vue version

--- packages/v-mapboxgl/bili.config.js
+++ packages/v-mapboxgl/bili.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 module.exports = {
     plugins: {
+        postcss: {
+          inject: false,
+        },
         typescript2: {
             tsconfigOverride: {
                 include: ['src'],
